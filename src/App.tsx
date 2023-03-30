e8f7e7e5c3b13fac85ee87e654990427e263798c
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-03-29 22:17:33 -0500

    set state for loading and render loading status

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMoviesHandler = async () => {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -21,6 +23,7 @@ function App() {
     });
 
     setMovies(transformedMovies);
+    setIsLoading(false);
   };
 
   return (
@@ -29,7 +32,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
       </section>
     </React.Fragment>
   );
